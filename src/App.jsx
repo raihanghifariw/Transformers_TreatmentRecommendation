4c3000bc93c1fecd9245e815010e8b5d8ec76f68
Pattayawat <pattayawat.poo@dome.tu.ac.th>
2026-01-08 19:48:32 +0700

    frontend

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Header from './components/Header.jsx'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+        <Header />
+        Hello Pragma
+      </div>
+    </>
+  )
+}
+
+export default App
